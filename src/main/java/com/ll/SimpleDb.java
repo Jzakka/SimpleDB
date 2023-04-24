2e7ca42eb319808bdd9365afdc0937d980f9ccd4
Jzakka <mouse4786@gmail.com>
2023-04-24 15:31:14 +0900

    feat: 쿼리 로깅

--- src/main/java/com/ll/SimpleDb.java
+++ src/main/java/com/ll/SimpleDb.java
@@ -1,21 +1,69 @@
 package com.ll;
 
-import lombok.AllArgsConstructor;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-@AllArgsConstructor
 public class SimpleDb {
-    private String host;
-    private String username;
-    private String password;
-    private String dbName;
+    private String url;
+    private final String username;
+    private final String password;
+    private final String dbName;
+    private boolean devMode;
 
-    public void setDevMode(boolean b) {
+    public SimpleDb(String host, String username, String password, String dbName) {
+        this.url = "jdbc:mysql://%s:3306/%s?serverTimezone=Asia/Seoul&useSSL=false".formatted(host,dbName);
+        this.username = username;
+        this.password = password;
+        this.dbName = dbName;
+    }
+
+    public void setDevMode(boolean devMode) {
+        this.devMode = devMode;
     }
 
     public void run(String query, Object... parameter) {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, username, password);
+
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            if (parameter.length >0) {
+                ps.setString(1, (String)parameter[0]);
+                ps.setString(2, (String)parameter[1]);
+                ps.setBoolean(3, (Boolean) parameter[2]);
+            }
+
+            if (this.devMode) {
+                logQuery(ps);
+            }
+
+            ps.execute();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private void logQuery(PreparedStatement ps) {
+        System.out.println("=======[QUERY_LOG]========");
+        System.out.println(ps.toString().split(": ")[1]);
+        System.out.println();
     }
 
     public Sql genSql() {
-        return null;
+        return new Sql();
     }
 }
